1be1f3702035bcb55c9ff978cd3441409aba7376
zhaoxiaowei <zhaoxiaowei@efy-tech.com>
2022-01-27 07:44:03 +0800

    update

--- linux-rk3399/Toolchain.cmake
+++ linux-rk3399/Toolchain.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(cross_triple "aarch64-unknown-linux-gnu")
+set(cross_triple "aarch64-linux-gnu")
 set(cross_root /usr/xcc/${cross_triple})
 
 set(CMAKE_C_COMPILER $ENV{CC})
@@ -15,6 +15,6 @@ set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
-set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
+# set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
