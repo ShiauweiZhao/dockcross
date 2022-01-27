10b4977466077d50f189c68b1bf604a682d47884
zhaoxiaowei <zhaoxiaowei@efy-tech.com>
2022-01-27 08:46:10 +0800

    update

--- linux-imx6ull/Toolchain.cmake
+++ linux-imx6ull/Toolchain.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(cross_triple "aarch64-linux-gnu")
+set(cross_triple "arm-linux-gnueabihf")
 set(cross_root /usr/xcc/${cross_triple})
 
 set(CMAKE_C_COMPILER $ENV{CC})
